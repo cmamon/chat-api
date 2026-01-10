40d0a255cd928e13edf287a50c3b37ac42a4870c
cmamon <christophe.quenette@gmail.com>
2026-01-10 13:24:54 +0100

    chore: update gitignore and remove dist from index

--- packages/database/dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const DATABASE_PACKAGE = "database";
-//# sourceMappingURL=index.d.ts.map
--- packages/database/dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DATABASE_PACKAGE = void 0;
-exports.DATABASE_PACKAGE = 'database';
-//# sourceMappingURL=index.js.map
